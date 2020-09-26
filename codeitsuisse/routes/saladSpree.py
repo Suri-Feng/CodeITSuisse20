57774ffaf391ad33c021a496e83c48c565118234
Suri Feng <0924suri@gmail.com>
2020-09-26 13:49:40 +0800

    Salad

--- codeitsuisse/routes/saladSpree.py
+++ codeitsuisse/routes/saladSpree.py
@@ -13,44 +13,23 @@ def evaluateSalad():
     data = request.get_json();
     logging.info("data sent for evaluation {}".format(data))
     #inputValue = data.get("input");
-    result = salad(data.get("number_of_salads"), data.get("salad_prices_street_map"))
+    result = price(data.get("number_of_salads"), data.get("salad_prices_street_map"))
     logging.info("My result :{}".format(result))
     return jsonify(result);
 
-def salad(num, myLists):
-    results = []
-    for i in range(len(myLists)):
-        salad_prices = myLists[i]
-        temp_res = prices(num, salad_prices)
-        if temp_res != -1:
-            results.append(temp_res)
-        results.sort()
-
-    if results == []:
-        ans = 0
-    else:
-        ans = results[0]
-    ansDict = {"result" : ans}
-    return ansDict
-
-
-def prices(num, price_list):
-    tots = []
-    counter = num
-    for i in range(len(price_list) - counter):
-        HasX = False
-        for j in range(counter):
-            if price_list[i + j] == "X":
-                HasX = True
-                break
-        if HasX == False:   
-            price = 0
-            for j in range(counter):
-                price += int(price_list[i+j])
-            tots.append(price)
-
-    tots.sort()
-    if tots == []:
-        return -1
-    else:
-        return tots[0]
+def price(number_of_salads, salad_prices_street_map):
+    result=0
+    for street in salad_prices_street_map:
+        if len(street)<=number_of_salads:
+            continue
+        else:
+            for i in range(len(street)-number_of_salads+1):
+                test=street[i:i+number_of_salads]
+                if "X" in test:
+                    continue
+                else:
+                    for i in range(number_of_salads):
+                        test[i]=int(test[i])
+                    if sum(test)< result or result==0:
+                        result=sum(test)
+    return {"result":result}
