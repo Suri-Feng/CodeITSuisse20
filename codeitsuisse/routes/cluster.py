349b8daa64514c8af99746c0482899fa9e65aa87
Suri Feng <0924suri@gmail.com>
2020-09-26 19:43:51 +0800

    cluster

--- codeitsuisse/routes/cluster.py
+++ codeitsuisse/routes/cluster.py
@@ -8,7 +8,7 @@ from codeitsuisse import app;
 logger = logging.getLogger(__name__)
 
 @app.route('/cluster', methods=['POST'])
-def evaluate():
+def evaluateCluster():
     data = request.get_json();
     logging.info("data sent for evaluation {}".format(data))
     result = cluster(data)
