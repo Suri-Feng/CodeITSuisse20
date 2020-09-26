945aeb316c316616a757429fde25b6959768c989
Suri Feng <0924suri@gmail.com>
2020-09-26 23:28:32 +0800

    changed func name - SocialDistancing

--- codeitsuisse/routes/socialdistance.py
+++ codeitsuisse/routes/socialdistance.py
@@ -8,7 +8,7 @@ from codeitsuisse import app;
 logger = logging.getLogger(__name__)
 
 @app.route('/social_distancing', methods=['POST'])
-def evaluate():
+def evaluateSocialDistancing():
     data = request.get_json();
     logging.info("data sent for evaluation {}".format(data))
     result = ways(data.get("tests"))
