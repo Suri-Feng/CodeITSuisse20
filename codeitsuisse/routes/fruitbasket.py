afa75a413862e7860ef2e590d67b818de7dfa26e
Suri Feng <0924suri@gmail.com>
2020-09-26 14:49:25 +0800

    magical

--- codeitsuisse/routes/fruitbasket.py
+++ codeitsuisse/routes/fruitbasket.py
@@ -10,13 +10,13 @@ logger = logging.getLogger(__name__)
 @app.route('/fruitbasket', methods=['POST'])
 
 def evaluate():
-    data = request.get_json();
+    data = request.get_data();
     logging.info("data sent for evaluation {}".format(data))
-    #apple = data.get("maApple");
-    #watermelon = data.get("maWatermelon");
-    #banana = data.get("maBanana");
-    #result = guess(apple, watermelon, banana)
-    result = 200
+    apple = data["maApple"];
+    watermelon = data["maWatermelon"];
+    banana = data.get["maBanana"];
+    result = guess(apple, watermelon, banana)
+    #result = 200
     logging.info("My result :{}".format(result))
     return jsonify(result);
 
