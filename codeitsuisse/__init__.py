ebf2ce961cbd09bc7c99f6cc1f42daa475a3302c
Suri Feng <0924suri@gmail.com>
2020-09-27 01:00:02 +0800

    test

--- codeitsuisse/__init__.py
+++ codeitsuisse/__init__.py
@@ -6,3 +6,5 @@ import codeitsuisse.routes.saladSpree
 #import codeitsuisse.routes.fruitbasket
 import codeitsuisse.routes.cluster
 import codeitsuisse.routes.socialdistance
+import codeitsuisse.routes.gmo
+import codeitsuisse.routes.hedging
---
+++ codeitsuisse/routes/hedging.py
@@ -0,0 +1,20 @@
+import logging
+import json
+
+from flask import request, jsonify;
+
+from codeitsuisse import app;
+
+logger = logging.getLogger(__name__)
+
+@app.route('/square', methods=['POST'])
+def evaluateHedging():
+    data = request.get_json();
+    logging.info("data sent for evaluation {}".format(data))
+    result = response(data)
+    logging.info("My result :{}".format(result))
+    return jsonify(result);
+
+def response(status):
+    order=status["order"]
+    return {"order": order}
