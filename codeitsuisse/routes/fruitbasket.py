55ed268afb784846ccf78cf5debb28178e30f9fd
Suri Feng <0924suri@gmail.com>
2020-09-26 14:02:57 +0800

    Magical

---
+++ codeitsuisse/routes/fruitbasket.py
@@ -0,0 +1,25 @@
+import logging
+import json
+
+from flask import request, jsonify;
+
+from codeitsuisse import app;
+
+logger = logging.getLogger(__name__)
+
+@app.route('/square', methods=['POST'])
+def evaluate():
+    data = request.get_json();
+    logging.info("data sent for evaluation {}".format(data))
+    apple = data.get("maApple");
+    watermelon = data.get("maWatermelon");
+    banana = data.get("maBanana");
+    result = guess(apple, watermelon, banana)
+    logging.info("My result :{}".format(result))
+    return json.dumps(result);
+
+def guess(apple, watermelon, banana):
+    Wa = 10
+    Ww = 20
+    Wb = 20
+    return Wa*apple + Ww*watermelon + Wb*banana
