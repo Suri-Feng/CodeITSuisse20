fc5882a66bae8548dd2e6da61a26738616306f73
Suri Feng <0924suri@gmail.com>
2020-09-27 01:34:54 +0800

    fruit

--- codeitsuisse/routes/fruitbasket.py
+++ codeitsuisse/routes/fruitbasket.py
@@ -13,15 +13,15 @@ def evaluateFruitBasket():
     data = request.get_data();
     data = json.loads(data)
     logging.info("data sent for evaluation {}".format(data))
-    apple = data["maApple"];
-    watermelon = data["maWatermelon"];
-    banana = data.get["maBanana"];
-    result = guess(apple, watermelon, banana)
+    result = guess(data)
     #result = 200
     logging.info("My result :{}".format(result))
     return jsonify(result);
 
-def guess(apple, watermelon, banana):
+def guess(data):
+    apple = data["maApple"]
+    watermelon = data["maWatermelon"]
+    banana = data.get["maBanana"]
     Wa = 10
     Ww = 20
     Wb = 20
