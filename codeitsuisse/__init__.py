c330c6d5ae863daa7737de9a64ce33400d812bd7
Suri Feng <0924suri@gmail.com>
2020-09-26 19:52:23 +0800

    remove fruitbasket

--- codeitsuisse/__init__.py
+++ codeitsuisse/__init__.py
@@ -3,5 +3,5 @@ app = Flask(__name__)
 import codeitsuisse.routes.square
 import codeitsuisse.routes.revisitgeometry
 import codeitsuisse.routes.saladSpree
-import codeitsuisse.routes.fruitbasket
+#import codeitsuisse.routes.fruitbasket
 import codeitsuisse.routes.cluster
