38817af9a9ada325fea7e91777ea420cbd216bdb
Suri Feng <0924suri@gmail.com>
2020-09-27 01:31:17 +0800

    fruit

--- codeitsuisse/__init__.py
+++ codeitsuisse/__init__.py
@@ -3,7 +3,7 @@ app = Flask(__name__)
 import codeitsuisse.routes.square
 import codeitsuisse.routes.revisitgeometry
 import codeitsuisse.routes.saladSpree
-#import codeitsuisse.routes.fruitbasket
+import codeitsuisse.routes.fruitbasket
 import codeitsuisse.routes.cluster
 import codeitsuisse.routes.socialdistance
 import codeitsuisse.routes.gmo
---
+++ codeitsuisse/routes/fruitbasket.py
@@ -0,0 +1,29 @@
+import logging
+import json
+
+from flask import request, jsonify;
+
+from codeitsuisse import app;
+
+logger = logging.getLogger(__name__)
+
+@app.route('/fruitbasket', methods=['POST'])
+
+def evaluateFruitBasket():
+    data = request.get_data();
+    data = json.loads(data)
+    logging.info("data sent for evaluation {}".format(data))
+    apple = data["maApple"];
+    watermelon = data["maWatermelon"];
+    banana = data.get["maBanana"];
+    result = guess(apple, watermelon, banana)
+    #result = 200
+    logging.info("My result :{}".format(result))
+    return jsonify(result);
+
+def guess(apple, watermelon, banana):
+    Wa = 10
+    Ww = 20
+    Wb = 20
+    ans = Wa*apple + Ww*watermelon + Wb*banana
+    return str(0)
