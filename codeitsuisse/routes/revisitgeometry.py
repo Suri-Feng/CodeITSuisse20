c2db0fe76c060d9fc58c74465377de68d8ab0f8a
Suri Feng <0924suri@gmail.com>
2020-09-26 11:07:41 +0800

    TestGeometry

---
+++ codeitsuisse/routes/revisitgeometry.py
@@ -0,0 +1,26 @@
+import logging
+import json
+
+from flask import request, jsonify;
+
+from codeitsuisse import app;
+
+logger = logging.getLogger(__name__)
+
+@app.route('/revisitgeometry', methods=['POST'])
+def evaluateGeometry():
+    data = request.get_json();
+    logging.info("data sent for evaluation {}".format(data))
+    #inputValue = data.get("input");
+    result = []
+    for testCase in data:
+        result.append(geometry(testCase["shapeCoordinates"], testCase["lineCoordinates"]))
+
+
+    logging.info("My result :{}".format(result))
+    return json.dumps(result);
+
+
+
+def gemometry(x, y):
+    return []
