edb750b4e090212ae43bcc049042252eca105b70
Suri Feng <0924suri@gmail.com>
2020-09-27 09:52:01 +0800

    test

--- codeitsuisse/routes/pretick.py
+++ codeitsuisse/routes/pretick.py
@@ -5,11 +5,16 @@ from flask import request, jsonify;
 
 from codeitsuisse import app;
 
+import pandas as pd
+
 logger = logging.getLogger(__name__)
 
 @app.route('/pre-tick', methods=['POST'])
 def evaluatePretick():
-    data = request.get_csv();
+    #data = request.get_json();
+    data = request.get_data();
+    print(data)
+    data = pd.read_csv();
     logging.info("data sent for evaluation {}".format(data))
     print(type(data))
     result = "920"
